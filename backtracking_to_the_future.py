0ff3e2f171d50a009117dcf6a9ff63de858923d7
fcagnola <fede.cagnola@gmail.com>
2020-12-22 12:22:10 +0100

    fixed networkx version of processing and impact factor

--- backtracking_to_the_future.py
+++ backtracking_to_the_future.py
@@ -21,43 +21,50 @@
 
 import csv
 from networkx import DiGraph
+import pandas
 
 
 def process_citations(citations_file_path):
-    g = DiGraph()  # for citations a directed graph would be the best choice
+    g = DiGraph()  # creates directed graph
 
-    with open(citations_file_path, mode="r") as csv_file:  # opening csv in read-mode
+    with open(citations_file_path, mode="r") as csv_file:  # opens csv in read-mode
         reader = csv.DictReader(csv_file)
 
         for row in reader:    # loop through rows, each row representing a citation
-            g.add_node(row['citing'], creation=row['creation'])  # create the citing node, with attribute 'creation'
-            g.add_node(row['cited'])                             # create cited node
-            g.add_edge(row['citing'], row['cited'], timespan=row['timespan'])  # create edge with timespan as attribute
 
-    return g  # i wonder if it would be better to return the adj dictionary or a tuple of (nodes, edges)
+            g.add_node(row['citing'], creation=row['creation'])  # creates citing node w/ attribute 'creation'
+
+            g.add_node(row['cited'])                             # creates cited node
+
+            g.add_edge(row['citing'], row['cited'], timespan=row['timespan'])  # creates edge w/ attribute 'timespan'
+
+    return g  # might be better to return adjacency dict (.adj) or a tuple of (nodes, edges)
 
 
-def do_compute_impact_factor(data, dois, year):  # dois is a set, year is 4 digit string 'YYYY'
+# def process_citations_pandas(citations_file_path):
+#     data_frame = pandas.read_csv(citations_file_path, dtype=str)
+#     return data_frame
 
-    # data is in graph format, for this function only node data is required, so store that in a variable
-    nodes = data.nodes(data=True)
+
+def do_compute_impact_factor(data, dois, year):  # DOIs is a set, year is 4 digit string 'YYYY'
+    # data is in DiGraph format
 
     cit_counter = 0  # this will be the dividend
     pub = 0          # this will be the divisor
 
-    for node in nodes:  # loop through nodes and dois given as parameter
-        for doi in dois:
+    for doi in dois:
+        citing = data.predecessors(doi)  # this is the network of articles citing a given DOI
+        for identifier in list(citing):
+            if str(data.nodes[identifier]['creation'][:4]) == year:
+                cit_counter += 1  # keeps count of citations in given year
 
-            # if the node is a citing one, and creation-year is == YEAR and the citing node is in set of dois
-            if node[1] != {} and node[1]['creation'][:4] == year and node[0] == doi: # for efficiency testing, we should try inverting conditions
-                cit_counter += 1  # add one to the dividend
-
-            # if node is citing and year is year-1 or year-2 add 1 to the divisor
-            if len(node[1]) != 0:
-                if node[1]['creation'][:4] == str(int(year)-1) or node[1]['creation'][:4] == str(int(year)-2):
-                    pub += 1
-
-    return round((cit_counter / pub), 2)  # this tries to solve float errors or weird divisions
+        try:   # python raises a KeyError when node doesn't have a creation date, handle it with exception
+            if data.nodes[doi]['creation'][:4] == str(int(year)-1) or data.nodes[doi]['creation'][:4] == str(int(year)-2):
+                pub += 1          # keeps count of the DOIs published in y-1 and y-2
+        except KeyError:
+            pass
+    # print('dividing {} by {}'.format(cit_counter, pub)) # DEBUG
+    return round((cit_counter / pub), 2)  # try to solve float errors or weird divisions
 
 
 def do_get_co_citations(data, doi1, doi2):
@@ -81,9 +88,13 @@ def do_search(data, query, field):
 def do_filter_by_value(data, query, field):
     pass
 
-# print(do_compute_impact_factor(process_citations('citations_sample.csv'),
-#                                {'10.1007/s00134-019-05862-0',
-#                                 '10.3390/vaccines7040201',
-#                                 '10.3390/vaccines8040600',
-#                                 '10.3414/me14-05-0004'},
-#                                '2016'))
+print(do_compute_impact_factor(process_citations('citations_sample.csv'),
+                               {'10.3389/fpsyg.2016.01483',
+                                '10.1097/mop.0000000000000929',
+                                '10.1177/000313481107700711',
+                                '10.3414/me14-05-0004',
+                                '10.3928/01477447-20180123-06',
+                                '10.1002/ddr.21369',
+                                '10.3889/mmej.2015.50002',
+                                '10.1016/s0140-6736(97)11096-0'},
+                               '2016'))
