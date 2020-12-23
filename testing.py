3daf9a8f82772a12ec4b3b6bf5ac984f21007616
fcagnola <fede.cagnola@gmail.com>
2020-12-23 19:57:48 +0100

    debugging pandas version of impact factor

--- testing.py
+++ testing.py
@@ -17,17 +17,43 @@ def do_compute_impact_factor(data, dois, year):  # dois is a set, year is 4 digi
     cit_counter = 0  # value will be dividend
     pub = set()      # len will be divisor
 
-    # this is the index of the columns of the dataframe, which keeps track of which row we're in
-    for i in data.index:
-        cited = data.loc[i, 'cited']
-        citing = data.loc[i, 'citing']
-        creation = data.loc[i, 'creation']
-        for doi in dois:
-            if doi == cited and creation[:4] == year:
-                cit_counter += 1
-            if doi == citing and creation[:4] == str(int(year)-1) or creation[:4] == str(int(year)-2):
-                pub.add(doi)
-    return 'numeratore {}, denominatore {}, if {}'.format(cit_counter, len(pub), round(cit_counter/len(pub), 2))
+    for doi in dois: # loop through input DOIs
+
+        cited = data.set_index('cited')   # save data indexed by 'cited' column to local variable
+        try:                              # handle KeyError exception
+            creation = cited.loc[doi]['creation']  # this can either be a str or a pandas series
+            if type(creation) == pandas.core.series.Series: # if series, loop through rows
+                for i in creation:
+                    if i[:4] == year:
+                        cit_counter += 1
+            elif type(creation) == str:                     # otherwise just compare year of creation and input year
+                if creation[:4] == year:
+                    cit_counter += 1
+        except KeyError:
+            pass
+
+        citing = data.set_index('citing')  # save data indexed by 'citing' column to local variable
+        try:                               # handle KeyError exception
+            creation = citing.loc[doi]['creation']  # this can either be a str or a pandas series
+            if type(creation) == pandas.core.series.Series:
+                if int(creation.iloc[1][:4]) == (int(year))-1 or int(creation.iloc[0][:4]) == (int(year))-2:
+                    print('DEBUG: ', creation.iloc[0][:4], 'is == to either {} or {}'.format(str(int(year)-1), str(int(year)-2)))
+                    pub.add(doi)
+            elif type(creation) == str:
+                if int(creation[:4]) == (int(year))-1 or (int(year))-2:
+                    print('DEBUG: ', creation[:4], 'is == to either {} or {}'.format(str(int(year)-1), str(int(year)-2)))
+                    pub.add(doi)
+        except KeyError:
+            pass
+
+    try:
+        return 'numeratore {}, denominatore {}, if {}'.format(cit_counter, len(pub), round(cit_counter / len(pub), 2))
+    except ZeroDivisionError:
+        return 'ERROR:\n' \
+               'Could not compute Impact Factor, no DOI was created in y-1 or y-2\n' \
+               'please try with another year\n' \
+               '[encountered ZeroDivisionError] {}'.format(cit_counter)
+
 
 
 print(do_compute_impact_factor(process_citations_pandas(f),
