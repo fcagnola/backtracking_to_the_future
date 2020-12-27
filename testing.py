086c3cfa069ed88068a9c937a32ecfee8f24b12e
fcagnola <fede.cagnola@gmail.com>
2020-12-27 19:21:35 +0100

    FINALLY solved impact factor (pandas)

--- testing.py
+++ testing.py
@@ -8,54 +8,48 @@ f = '/Users/federicocagnola/PycharmProjects/backtracking_to_the_future/citations
 
 
 def process_citations_pandas(citations_file_path):
-    data_frame = pandas.read_csv(citations_file_path)
+    data_frame = pandas.read_csv(citations_file_path, dtype="string")
     return data_frame
-
-c = process_citations_pandas(f)
-#print(c.head())
 
 
 def do_compute_impact_factor(data, dois, year):  # dois is a set, year is 4 digit string 'YYYY'
 
+    if len(dois) == 0:
+        return 'Please insert a valid set of DOIs'
+    if type(year) == int:
+        return 'Please provide a year in string format: "YYYY"'
     cit_counter = 0  # value will be dividend
-    pub = set()      # len will be divisor
+    pub = set()  # len will be divisor
 
-    for doi in dois: # loop through input DOIs
+    for doi in dois:                            # loop through input DOIs
 
-        cited = data.set_index('cited')   # save data indexed by 'cited' column to local variable
-        try:                              # handle KeyError exception
-            creation = cited.loc[doi]['creation']  # this can either be a str or a pandas series
-            if type(creation) == pandas.core.series.Series: # if series, loop through rows
-                for i in creation:
-                    if i[:4] == year:
-                        cit_counter += 1
-            elif type(creation) == str:                     # otherwise just compare year of creation and input year
-                if creation[:4] == year:
+        data.set_index('cited', inplace=True)   # index dataframe by 'cited' column
+        try:
+            created = data.loc[doi, 'creation'] # lookup all mentions of the DOI in the input set, column 'creation'
+            for i in created:
+                if i[:4] == year:
                     cit_counter += 1
         except KeyError:
             pass
+        data.reset_index(inplace=True)          # reset original integer index
 
-        citing = data.set_index('citing')  # save data indexed by 'citing' column to local variable
-        try:                               # handle KeyError exception
-            creation = citing.loc[doi]['creation']  # this can either be a str or a pandas series
-            if type(creation) == pandas.core.series.Series:
-                if int(creation.iloc[1][:4]) == (int(year))-1 or int(creation.iloc[0][:4]) == (int(year))-2:
-                    print('DEBUG: ', creation.iloc[0][:4], 'is == to either {} or {}'.format(str(int(year)-1), str(int(year)-2)))
-                    pub.add(doi)
-            elif type(creation) == str:
-                if int(creation[:4]) == (int(year))-1 or (int(year))-2:
-                    print('DEBUG: ', creation[:4], 'is == to either {} or {}'.format(str(int(year)-1), str(int(year)-2)))
-                    pub.add(doi)
+        data.set_index('citing', inplace=True)  # index dataframe by 'citing' column
+        try:
+            creation = data.loc[doi, 'creation'][0] # select creation year of the first match (all rows will be identical)
+
+            if len(creation) == 1:  # if there was a single row creation contains only '2', the first elem of year
+                creation = data.loc[doi, 'creation'][:4] # correct problem of the line before
+
+            if creation == str(int(year)-1) or creation == str(int(year)-2):
+                pub.add(doi)
         except KeyError:
             pass
-
+        data.reset_index(inplace=True)          # reset original integer index
     try:
-        return 'numeratore {}, denominatore {}, if {}'.format(cit_counter, len(pub), round(cit_counter / len(pub), 2))
+        return round(cit_counter/len(pub), 2)
     except ZeroDivisionError:
-        return 'ERROR:\n' \
-               'Could not compute Impact Factor, no DOI was created in y-1 or y-2\n' \
-               'please try with another year\n' \
-               '[encountered ZeroDivisionError] {}'.format(cit_counter)
+        return "Could not compute impact factor: no DOIs pointed to objects published in \n" \
+               "year-1 or year-2. Please try with another input set or year."
 
 
 
@@ -69,4 +63,16 @@ print(do_compute_impact_factor(process_citations_pandas(f),
                                 '10.3889/mmej.2015.50002',      # created 2015 Y
                                 '10.1016/s0140-6736(97)11096-0'}, # no creation
                                '2016'))
+print(do_compute_impact_factor(process_citations_pandas(f),
+                               set(), '2016'))
+print(do_compute_impact_factor(process_citations_pandas(f),
+                                {'10.3389/fpsyg.2016.01483',     # created 2016 N
+                                '10.1097/mop.0000000000000929', # created 2020 N
+                                '10.1177/000313481107700711',   # created 2011 N
+                                '10.3414/me14-05-0004',         # created 2014 Y
+                                '10.3928/01477447-20180123-06', # created 2018 N
+                                '10.1002/ddr.21369',            # created 2016 N
+                                '10.3889/mmej.2015.50002',      # created 2015 Y
+                                '10.1016/s0140-6736(97)11096-0'},
+                                2016))
 
