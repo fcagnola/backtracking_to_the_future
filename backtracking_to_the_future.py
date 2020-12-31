a9e0a27b921d1501220f993bb01a782f0f6e3728
fcagnola <fede.cagnola@gmail.com>
2020-12-31 18:40:56 +0100

    committed impact factor function

--- backtracking_to_the_future.py
+++ backtracking_to_the_future.py
@@ -21,12 +21,42 @@
 
 import pandas
 
+
 def process_citations(citations_file_path):
     data_frame = pandas.read_csv(citations_file_path, dtype={'citing': str, 'cited': str, 'timespan': str}, parse_dates=['creation'])
     return data_frame
 
-def do_compute_impact_factor(data, dois, year):
-    pass
+
+def do_compute_impact_factor(data, dois, year):  # DOIs is a set, year is 4 digit string 'YYYY'
+    if len(dois) == 0:
+        return 'Please insert a valid set of DOIs'
+    if type(year) == int:
+        return 'Please provide a year in string format: "YYYY"'
+
+    num = 0
+    denom = 0
+
+    # selecting only rows with year 'year'
+    data_year = data.loc[data['creation'].dt.year == int(year)]
+
+    # selecting only rows with previous two years: concatenate
+    data_previous_two_years = pandas.concat([data.loc[data['creation'].dt.year == (int(year) - 1)], data.loc[data['creation'].dt.year == (int(year) - 2)]])
+
+    for doi in dois:
+        # selecting rows with doi == cited and adding the length of this table to num
+        data_year_cited = data_year.loc[data_year['cited'] == doi]
+        num += len(data_year_cited)
+
+        # selecting rows with doi == citing and adding the length of this table to denom
+        data_previous_years_citing = data_previous_two_years.loc[data_previous_two_years['citing'] == doi]
+        denom += len(data_previous_years_citing)
+
+    try:
+        return round(num / denom, 2)
+    except ZeroDivisionError:
+        return "Could not compute impact factor: no DOIs pointed to objects published in \n" \
+               "year-1 or year-2. Please try with another input set or year."
+
 
 def do_get_co_citations(data, doi1, doi2):
     pass
