9930a58a80d9631ca3ce3422dc430771160618c8
fcagnola <fede.cagnola@gmail.com>
2021-01-02 20:06:33 +0100

    new efficiency test for networkx

--- efficiency_testing.py
+++ efficiency_testing.py
@@ -1,144 +1,78 @@
-# EFFICIENCY TESTING: PANDAS VS NETWORKX DATA STRUCTURE
+# EFFICIENCY TESTING:
 
-# RESULTS:
-# The execution time of function impact_factor_fede() is 9.074071624
-# The execution time of function impact_factor_constance() is 3.8846544460000008
-# The most efficient function is impact_factor_constance() that runs in 3.8846544460000008 seconds.
-# The least efficient function is impact_factor_fede() that runs in 9.074071624 seconds.
 
 setup = '''
 import pandas
+from networkx import DiGraph, from_pandas_edgelist
+import numpy as np
+import re
 
 def process_citations(citations_file_path):
-    data_frame = pandas.read_csv(citations_file_path, dtype="string")
+    # processing through pandas' read.csv function: date parsing is necessary for easier handling of 'creation' column
+    data_frame = pandas.read_csv(citations_file_path, dtype={'citing': str, 'cited': str, 'timespan': str}, parse_dates=['creation'])
     return data_frame
 
 dataframe = process_citations('/Users/federicocagnola/PycharmProjects/backtracking_to_the_future/citations_sample.csv')
 
-def process_citations_date(citations_file_path):
-    data_frame = pandas.read_csv(citations_file_path, dtype={'citing': str, 'cited': str, 'timespan': str},
-                                 parse_dates=['creation'])
-    return data_frame
+def do_get_citation_network(data, start, end):  # F
 
-df = process_citations_date('/Users/federicocagnola/PycharmProjects/backtracking_to_the_future/citations_sample.csv')
+    # input validation
+    if int(end) < int(start):
+        return "Invalid input: enter an end year greater than the start"
 
-def impact_factor_fede(data, dois, year):  # dois is a set, year is 4 digit string 'YYYY'
+    # filter data on given time window start->end
+    i = int(start)      # iterator, set as ==start, then 1 is added each iter until 'end' is reached
+    ls_dfs = []         # list will contain dataframes for each year in time window
+    while i != (int(end) + 1):
+        ls_dfs.append(data[data['creation'].dt.year == i])  # add only rows with creation year == iterator
+        i += 1                                              # increment iterator
 
-    if len(dois) == 0:
-        return 'Please insert a valid set of DOIs'
-    if type(year) == int:
-        return 'Please provide a year in string format: "YYYY"'
-    cit_counter = 0                             # value will be dividend
-    pub = set()                                 # len will be divisor
+    # concatenate dataframes, create dataframe for whole timewindow (start>end)
+    d = pandas.concat(ls_dfs)
+    # compute a 'creation_cited' column with dates for the cited DOIs, through an ancillary function
+    d['creation_cited'] = d[['creation', 'timespan']].apply(do_compute_date_column, axis=1)
+    # NEED TO REMOVE DOIs WHICH HAVE CREATION CITED VALUE != TIMEWINDOW START-END
 
-    for doi in dois:                            # loop through input DOIs
+    # create actual Directed Network through networkx
+    graph = from_pandas_edgelist(d, source='citing', target='cited', create_using=DiGraph)
 
-        data.set_index('cited', inplace=True)   # index dataframe by 'cited' column
-        try:
-            created = data.loc[doi, 'creation'] # lookup all mentions of the DOI in the input set, column 'creation'
-            for i in created:
-                if i[:4] == year:
-                    cit_counter += 1
-        except KeyError:
-            pass
-        data.reset_index(inplace=True)          # reset original integer index
+    return graph
+def do_compute_date_column(row):  # this function takes a pd.Series as input (row of a pd.DataFrame)
 
-        data.set_index('citing', inplace=True)  # index dataframe by 'citing' column
+    timespan = row['timespan']    # the elem at index 'timespan' is the timespan in 'P_Y_M_D' format
+    creation = row['creation']    # the elem at index 'creation' is the date of creation in 'YYYY-MM-DD' format
 
-        try:
-            creation = data.loc[doi, 'creation'][0] # select creation year of the first match (all rows will be identical)
+    negative = False              # timespan could be negative, in that case the computation should be reversed
+    if timespan[0] == "-":
+        negative = True
 
-            if len(creation) == 1:  # if result was a single row, creation contains only '2', the first char of 'YYYY'
-                creation = data.loc[doi, 'creation'][:4]                      # correct problem of the line before
+    timespan = timespan.strip('PD')     # i remove the 'P' at the beginning and the 'D' at the end of the timespan
+    ls = re.split('[YM]', timespan)     # then a list is created in [yy, mm, dd] format
 
-            if creation == str(int(year)-1) or creation == str(int(year)-2):
-                pub.add(doi)
-        except KeyError:
-            pass
-        data.reset_index(inplace=True)          # reset original integer index
-    try:
-        return round(cit_counter/len(pub), 2)
-    except ZeroDivisionError:
-        return "Could not compute impact factor: no DOIs pointed to objects published in "\n" year-1 or year-2. Please try with another input set or year."
-               
-def impact_factor_constance(data, dois, year):
-    num = 0
-    denom = 0
-    
-    # selecting only rows with year 'year'
-    data_year = data.loc[data['creation'].str.contains(year)]
+    date_column_value = creation        # this will be the return value: computed creation time for cited DOI
+    if not negative:
+        for idx, value in enumerate(ls):    # loop through elements in the list (there could be YY, YY-MM or YY-MM-DD)
+            if idx == 0:                    # first elem will always be year: compute year by subtraction
+                date_column_value = date_column_value - np.timedelta64(value, 'Y')
+            elif idx == 1 and value != '':  # second elem will always be month: compute month by subtraction
+                date_column_value = date_column_value - np.timedelta64(value, 'M')
+            elif idx == 2 and value != '':  # third elem will always be day: compute day by subtraction
+                date_column_value = date_column_value - np.timedelta64(value, 'D')
+    else:
+        for idx, value in enumerate(ls):    # loop through elements in the list (there could only be YY or YY-MM)!
+            if idx == 0:                    # first elem will always be year: compute year by subtraction
+                date_column_value = date_column_value + np.timedelta64(value, 'Y')
+            elif idx == 1 and value != '':  # second elem will always be month: compute month by subtraction
+                date_column_value = date_column_value + np.timedelta64(value, 'M')
+            elif idx == 2 and value != '':  # third elem will always be day: compute day by subtraction
+                date_column_value = date_column_value + np.timedelta64(value, 'D')
 
-    #selecting only rows with previous two years
-    data_previous_two_years = data.loc[data['creation'].str.contains(str(int(year)-1)+'|'+str(int(year)-2))]
-    for doi in dois:
-
-        #selecting rows with doi == cited and adding the length of this table to num
-        data_year_cited = data_year.loc[data_year['cited'] == doi]
-        num += len(data_year_cited)
-
-        #selecting rows with doi == citing and adding the length of this table to denom
-        data_previous_years_citing = data_previous_two_years.loc[data_previous_two_years['citing'] == doi]
-        denom += len(data_previous_years_citing)
-
-    return round(num/denom, 2)
-
-def do_compute_impact_factor(data, dois, year):  # DOIs is a set, year is 4 digit string 'YYYY'
-    if len(dois) == 0:
-        return 'Please insert a valid set of DOIs'
-    if type(year) == int:
-        return 'Please provide a year in string format: "YYYY"'
-
-    num = 0
-    denom = 0
-
-    # selecting only rows with year 'year'
-    data_year = data.loc[data['creation'].dt.year == int(year)]
-
-    # selecting only rows with previous two years
-    data_previous_two_years = pandas.concat([data.loc[data['creation'].dt.year == (int(year) - 1)], data.loc[data['creation'].dt.year == (int(year) - 2)]])
-
-    for doi in dois:
-        # selecting rows with doi == cited and adding the length of this table to num
-        data_year_cited = data_year.loc[data_year['cited'] == doi]
-        num += len(data_year_cited)
-
-        # selecting rows with doi == citing and adding the length of this table to denom
-        data_previous_years_citing = data_previous_two_years.loc[data_previous_two_years['citing'] == doi]
-        denom += len(data_previous_years_citing)
-
-    try:
-        return round(num / denom, 2)
-    except ZeroDivisionError:
-        return "Could not compute impact factor: no DOIs pointed to objects published in year-1 or year-2. Please try with another input set or year."
+    return date_column_value
 '''
-l_statements = ['''impact_factor_fede(dataframe, {'10.3389/fpsyg.2016.01483',     # created 2016 N
-                                '10.1097/mop.0000000000000929', # created 2020 N
-                                '10.1177/000313481107700711',   # created 2011 N
-                                '10.3414/me14-05-0004',         # created 2014 Y
-                                '10.3928/01477447-20180123-06', # created 2018 N
-                                '10.1002/ddr.21369',            # created 2016 N
-                                '10.3889/mmej.2015.50002',      # created 2015 Y
-                                '10.1016/s0140-6736(97)11096-0'}, "2016")''',
-                '''impact_factor_constance(dataframe, {'10.3389/fpsyg.2016.01483',     # created 2016 N
-                                '10.1097/mop.0000000000000929', # created 2020 N
-                                '10.1177/000313481107700711',   # created 2011 N
-                                '10.3414/me14-05-0004',         # created 2014 Y
-                                '10.3928/01477447-20180123-06', # created 2018 N
-                                '10.1002/ddr.21369',            # created 2016 N
-                                '10.3889/mmej.2015.50002',      # created 2015 Y
-                                '10.1016/s0140-6736(97)11096-0'}, "2016")''',
-                '''do_compute_impact_factor(df, {'10.3389/fpsyg.2016.01483',     # created 2016 N
-                                '10.1097/mop.0000000000000929', # created 2020 N
-                                '10.1177/000313481107700711',   # created 2011 N
-                                '10.3414/me14-05-0004',         # created 2014 Y
-                                '10.3928/01477447-20180123-06', # created 2018 N
-                                '10.1002/ddr.21369',            # created 2016 N
-                                '10.3889/mmej.2015.50002',      # created 2015 Y
-                                '10.1016/s0140-6736(97)11096-0'}, "2016")''']
-l_functions = ["impact_factor_fede()", "impact_factor_constance()", "do_compute_impact_factor()"]
+l_statements = ['''do_get_citation_network(dataframe, "2018", "2020")''']
+l_functions = ["do_get_citation_network()"]
 
 #######################################################################################################################
-
 
 import timeit
 
@@ -163,4 +97,4 @@ def multipleniceevaluator(listoffunctions, setup, listofstatements, number=1000)
         print("Invalid input, the length of the lists you have put as input is different")
 
 
-print(multipleniceevaluator(l_functions, setup, l_statements, 500))
+print(multipleniceevaluator(l_functions, setup, l_statements, 100))
