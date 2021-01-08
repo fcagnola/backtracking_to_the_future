f705f6c8b3d421c1ab232a23fcf668368f13698c
fcagnola <fede.cagnola@gmail.com>
2021-01-08 17:25:00 +0100

    tested impact factor with custom sample

--- execution_example.py
+++ execution_example.py
@@ -19,11 +19,16 @@ from bibengine import BibliometricEngine
 #testing files: /Users/federicocagnola/Desktop/testing_csv.csv OR citations_sample.csv
 
 # create a new object of the class 'BibliometricEngine' specifying the input CSV files to process
-my_be = BibliometricEngine('citations_sample.csv')
+my_be = BibliometricEngine('~/Desktop/testing_csv.csv')
 # the data in the object can be accessed through the .data method specified in the __init__ function
-print(my_be.data.info())
+# print(my_be.data.info())
+
+# for i in range(2001, 2020):
+#     print("------")
+#     print(i)
+#     print(my_be.compute_impact_factor({'10.3389/fpsyg.2016.01483', '10.1097/mop.0000000000000929', '10.1177/000313481107700711','10.3414/me14-05-0004','10.3928/01477447-20180123-06','10.1002/ddr.21369','10.3889/mmej.2015.50002','10.1016/s0140-6736(97)11096-0'}, str(i)))
 
 for i in range(2001, 2020):
-    print("------")
+    print("-------------------------------------------------------")
     print(i)
-    print(my_be.compute_impact_factor({'10.3389/fpsyg.2016.01483', '10.1097/mop.0000000000000929', '10.1177/000313481107700711','10.3414/me14-05-0004','10.3928/01477447-20180123-06','10.1002/ddr.21369','10.3889/mmej.2015.50002','10.1016/s0140-6736(97)11096-0'}, str(i)))
+    print(my_be.compute_impact_factor({'federico', 'giulia', 'ivan', 'luisa', 'constance', 'bruno'}, str(i)))
